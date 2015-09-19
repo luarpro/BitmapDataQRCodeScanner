bf0b0eb595176f12c9b58baf3f59bff3575db0f8
Luar Yen <luar.yen@gmail.com>
2015-09-20 00:40:43 +0800

    update

--- ane/build_ane.bat
+++
@@ -1,9 +0,0 @@
-::edit Flex SDK bin folder path
-set FLEX_SDK=C:\Program Files (x86)\FlashDevelop\Tools\flexsdk
-
-::do not edit below
-
-@echo off
-echo "build com.kcly.ane.bitmapdataqrcodescanner.ane..."
-"%FLEX_SDK%\bin\adt.bat" -package -target ane com.kcly.ane.bitmapdataqrcodescanner.ane extension.xml -swc com.kcly.ane.bitmapdataqrcodescanner.swc -platform Android-ARM library.swf libBitmapDataQRCodeScanner.jar libs res -platform iPhone-ARM library.swf libBitmapDataQRCodeScanner.a platform.xml
-echo "Done!"
